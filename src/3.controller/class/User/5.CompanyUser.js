97146d00b6204762b011ee1ac289facf4d313491
Rôney Fischer <fischer.roney@gmail.com>
2022-11-04 10:50:35 -0300

    created 5.CompanyUser.js

---
+++ src/3.controller/class/User/5.CompanyUser.js
@@ -0,0 +1,11 @@
+import AdmUser from "./3.AdmUser"
+
+class ComapnyUser extends AdmUser{
+
+    //excluir usuários de terceiros
+    //alterar ROLE de qualquer usuário
+    //criar usuários de terceiros
+
+}
+
+export default ComapnyUser;
