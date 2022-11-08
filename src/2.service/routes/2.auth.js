45daaf0349d90c150516e20aea63a863e978dc2f
Rôney Fischer <115057333+Roneyfischer@users.noreply.github.com>
2022-11-08 13:13:03 -0300

    Update 2.auth.js

--- src/2.service/routes/2.auth.js
+++ src/2.service/routes/2.auth.js
@@ -5,40 +5,29 @@ import jwt from "jsonwebtoken";
 const auth = express.Router();
 
 auth.post("/", async (req, res) => {
-  //desfazer esse macarrão
+
   console.log("> [route.auth]");
+
   const user = new AdmUser();
-  const typeRequisition = req.body.type;
-  //seta o cookie se colcoar login e dados do register
-  if (typeRequisition == "login" || typeRequisition == "register") {
-    console.log("> [route.auth] Requisition ok.");
 
-    const operation = await user[req.body.type](req.body, res);
-    console.log("> [route.auth] Result login:" + operation.status);
+const operation = await user[req.body.type](req.body, res);
 
-    if (operation.status) {
-      console.log("> [route.auth] answered for to front-end.");
-      const { cpf } = req.body;
-      const token = jwt.sign({ userId: cpf }, process.env.JWT_KEY, {
-        expiresIn: 30000,
-      });
+    if(!operation.status) {
+      return res.status(401).json({ return: operation.message });
+    }
 
-      return res
-        .cookie("access_token", token, {
+    if(operation.status) {
+    return res
+        .cookie("access_token", operation.token, {
           secure: true,
           sameSite: "none",
           expire: 500000,
         })
         .status(200)
-        .json({ return: operation.status, message: operation.message });
+        .json({ return: operation.status, message: operation.message, token: token });
     }
+   
 
-    return res.status(401).json({ return: operation.message });
-  }
-  console.log("> [route.auth] Fail requisition.");
-  return res
-    .status(401)
-    .json({ status: false, message: "Incorrect requisition" });
 });
 
 export default auth;
