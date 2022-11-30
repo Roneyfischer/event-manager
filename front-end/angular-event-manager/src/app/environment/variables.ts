27c1dd4eccb00ed84cebd2b015329b8ae940cb7a
Rôney Fischer <fischer.roney@gmail.com>
2022-11-30 09:53:43 -0300

    same

--- front-end/angular-event-manager/src/app/environment/variables.ts
+++ front-end/angular-event-manager/src/app/environment/variables.ts
@@ -1,8 +1,6 @@
-
 const variables = {
-    ApiUrl: `https://fischerserver.shop/`
+  // ApiUrl: `https://fischerserver.shop/`
+  ApiUrl: `http://127.0.0.1:3333`,
+};
 
-
-}
-
-export default variables
+export default variables;
